25a61ab24db5ba149407c73203fbeecb0c71d59e
kostin <kostin@ua2web.com>
2022-04-19 19:28:45 +0300

    resolved pylint for geoname

--- gpsfun/gpsfun/geoname/urls.py
+++ gpsfun/gpsfun/geoname/urls.py
@@ -1,14 +1,14 @@
+"""
+urls for application geoname
+"""
+
 from django.urls import path
 from gpsfun.geoname.views import (
-    get_country_regions, get_region_cities, get_waypoint_address
-)
+    get_country_regions, get_region_cities, get_waypoint_address)
 
 
 urlpatterns = [
-    path('change/country/', get_country_regions,
-        name="get-country-regions"),
-    path('change/region/', get_region_cities,
-        name="get-region-cities"),
-    path('address/', get_waypoint_address,
-        name="get-waypoint-address"),
+    path('change/country/', get_country_regions, name="get-country-regions"),
+    path('change/region/', get_region_cities, name="get-region-cities"),
+    path('address/', get_waypoint_address, name="get-waypoint-address"),
 ]
--- gpsfun/gpsfun/geoname/views.py
+++ gpsfun/gpsfun/geoname/views.py
@@ -1,4 +1,6 @@
-# coding: utf-8
+"""
+views for application geoname
+"""
 
 from django.utils import translation
 
@@ -10,47 +12,48 @@ from gpsfun.main.GeoName.models import (GeoCountry, GeoCountryAdminSubject,
 
 @accept_ajax
 def get_country_regions(request):
-    rc = {'status': 'ERR',
-          }
+    """ get country regions """
+    result = {'status': 'ERR'}
 
     country_code = request.GET.get('country')
 
     if not country_code:
-        return rc
+        return result
     country = get_object_or_none(GeoCountry, iso=country_code)
     address = 'Ukraine, Kharkov'
     if country:
-        address = '%s,%s' % (country.name, country.capital)
+        address = f'{country.name},{country.capital}'
 
     regions = GeoCountryAdminSubject.objects.filter(country_iso=country_code)
     regions = regions.exclude(name='').exclude(name__isnull=True)
     regions = regions.values('code', 'name').order_by('name')
     regions = list(regions)
 
-    rc['regions'] = sorted(regions, key=lambda x: x['name'])
-    rc['address'] = address
-    rc['status'] = 'OK'
+    result['regions'] = sorted(regions, key=lambda x: x['name'])
+    result['address'] = address
+    result['status'] = 'OK'
 
-    return rc
+    return result
 
 
 @accept_ajax
 def get_region_cities(request):
-    rc = {'status': 'ERR',
-          }
+    """ get region cities """
+    result = {'status': 'ERR'}
 
     country_code = request.GET.get('country')
     region_code = request.GET.get('region')
     if not (country_code and region_code):
-        return rc
+        return result
 
     country = get_object_or_none(GeoCountry, iso=country_code)
-    region = get_object_or_none(GeoCountryAdminSubject,
-                                code=region_code,
-                                country_iso=country_code)
+    region = get_object_or_none(
+        GeoCountryAdminSubject,
+        code=region_code,
+        country_iso=country_code)
     address = 'Ukraine, Kharkov'
     if country is not None and region is not None:
-        address = '%s,%s' % (country.name, region.name)
+        address = f'{country.name},{region.name}'
 
     cities = GeoCity.objects.filter(country=country_code)
     cities = cities.filter(admin1=region_code)
@@ -60,42 +63,38 @@ def get_region_cities(request):
                'name': city.localized_name(translation.get_language())} \
               for city in cities]
 
-    rc['cities'] = sorted(cities, key=lambda x: x['name'])
-    rc['address'] = address
-    rc['status'] = 'OK'
+    result['cities'] = sorted(cities, key=lambda x: x['name'])
+    result['address'] = address
+    result['status'] = 'OK'
 
-    return rc
+    return result
 
 
 @accept_ajax
 def get_waypoint_address(request):
-    rc = {'status': 'ERR',
-          }
+    """ get waypoint address """
+    result = {'status': 'ERR'}
     country_code = request.GET.get('country')
     region_code = request.GET.get('region')
     city_code = request.GET.get('city')
     if not country_code or country_code == 'NONE':
-        return rc
+        return result
 
-    rc['status'] = 'OK'
+    result['status'] = 'OK'
     country = country_by_code(country_code)
     if not region_code or region_code == 'NONE':
-        rc['address'] = country or ''
-        return rc
+        result['address'] = country or ''
+        return result
 
     region = region_by_code(country_code, [region_code])
     if not city_code or city_code == 'NONE':
-        rc['address'] = '%s %s' % (country or '',
-                                   region or '')
-        return rc
+        result['address'] = f"{country or ''} {region or ''}"
+        return result
 
     city = get_city_by_geoname(city_code)
     if city:
-        rc['address'] = city.address_string()
-        return rc
-    else:
-        rc['address'] = '%s %s' % (country or '',
-                                   region or '')
-        return rc
+        result['address'] = city.address_string()
+        return result
+    result['address'] = f"{country or ''} {region or ''}"
 
-    return rc
+    return result
