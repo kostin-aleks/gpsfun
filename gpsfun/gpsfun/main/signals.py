d5baec3f61a825c3faf29eb97ba087ac56162c76
kostin <kostin@ua2web.com>
2022-04-21 22:48:34 +0300

    fixed import

--- gpsfun/gpsfun/main/signals.py
+++ gpsfun/gpsfun/main/signals.py
@@ -1,11 +1,11 @@
 """
 signals
 """
-from gpsfun.main.User.models import GPSFunUser
 
 
 def create_custom_user(sender, instance, signal, created, **kwargs):
     """When user is created also create a custom user."""
+    from gpsfun.main.User.models import GPSFunUser
 
     if created:
         GPSFunUser.objects.create(user=instance)
